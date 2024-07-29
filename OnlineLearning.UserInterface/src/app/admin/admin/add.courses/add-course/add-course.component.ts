1ab4bbb4ef0d54a376a49a285763477d7c48284a
Nayanajith <93811nnhewa@gmail.com>
2024-07-29 23:08:56 +0530

    Did some changes

--- OnlineLearning.UserInterface/src/app/admin/admin/add.courses/add-course/add-course.component.ts
+++ OnlineLearning.UserInterface/src/app/admin/admin/add.courses/add-course/add-course.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { course } from 'src/app/Models/course';
+import { CourseService } from 'src/app/services/course/course.service';
 
 @Component({
   selector: 'app-add-course',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AddCourseComponent {
 
+  constructor(private courseService:CourseService){}
+
+  course :course = {} as course;
+  submit(form:any){
+    debugger
+    this.course.courseName = form.value.coursename,
+    this.course.courseCategory = form.value.courseCategory,
+    this.course.courseCategory = form.value.courseCategory
+    this.courseService.createCourse(this.course).subscribe((res:any)=>{
+      decodeURI
+      return res;
+    })
+  }
 }
--- OnlineLearning.UserInterface/src/app/services/course/course.service.ts
+++ OnlineLearning.UserInterface/src/app/services/course/course.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClientModule,HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { course } from 'src/app/Models/course';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,15 @@ import { Observable } from 'rxjs';
 export class CourseService {
 
   constructor(private http:HttpClient) { }
-  
+
   url = "https://localhost:7042/api/Course/courses"
+
   getAllcourses():Observable<any>{
     return this.http.get(this.url)
+  }
+
+
+  createCourse(course:course):Observable<any>{
+    return this.http.post("https://localhost:7042/api/Course",course);
   }
 }
